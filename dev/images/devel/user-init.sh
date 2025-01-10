28f85100df8e9afc02705dc6fb5a80bd7e4a70e3
Robert Strickland <strickland.rbls@gmail.com>
2025-01-10 07:11:38 -0600

    Fixed docker windows issue

--- dev/images/devel/user-init.sh
+++ dev/images/devel/user-init.sh
@@ -2,14 +2,8 @@
 USER=dev
 DIR_USER_HOME=/home/$USER
 
-[[ ! -e "${DIR_USER_HOME}" ]] && {
-    echo "Creating user home: ${DIR_USER_HOME}"
-    mkdir -p ${DIR_USER_HOME}/BinMorph/{core,tests,thirdparty,examples}
-    echo "Creating dir: ${DIR_USER_HOME}/.local/{bin,share,lib}"
-    mkdir -p ${DIR_USER_HOME}/.local/{bin,share,lib}
-    echo "User home setup complete"
-} ;
-echo "HERE: $?"
+[[ ! -e "${DIR_USER_HOME}" ]] && { mkdir -p ${DIR_USER_HOME}/BinMorph/{core,tests,thirdparty,examples} ; } ;
+
 [[ $(cat /etc/passwd | grep dev) != 0 ]] && {
     echo "Creating user: ${USER}"
     echo "${USER}:x:1000:1000:${USER}:${DIR_USER_HOME}:/bin/bash" >> /etc/passwd
